ca7f5afdeee3f3c724c1440d73a4145f5afdb853
Christophe <christophe.lombart@gmail.com>
2019-07-11 13:02:47 +0200

    update package versions

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ const { findContent } = require('../index.js');
 
 const writeFile = util.promisify(fs.writeFile);
 
-const url = 'https://www.purina.fr/chiens/dressage-comportement/dressage-chien/techniques-dressage';
+const url = 'https://www.amstelgeld.nl/goedkoop-geld-lenen/';
 
 describe('Test Find Content', async () => {
   let $ = null;
